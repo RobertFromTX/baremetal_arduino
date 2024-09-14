29b42b9e509321225ed3e40c92c262d771022266
RobertFromTX <robc.btx@gmail.com>
2024-09-14 00:11:25 -0500

    first commit

---
+++ blink_example/led.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <util/delay.h>
+//don't know why I have to add below to get rid of red sguiggles, but works without it
+// #include <avr/io43u32x.h>
+// #include <avr/io90pwm161.h>
+
+
+int main(void)
+{
+    //set PORTB5 as an output
+    DDRB = DDRB | (1<<DDB5);
+
+    while(1)
+    {
+        //set PORT B5
+        PORTB = PORTB | (1 << PORTB5);
+
+        //wait 
+        _delay_ms(1000);//does no operations until 1000ms has elapsed
+
+        //unset PORTB5
+        PORTB = PORTB & ~(1 << PORTB5);
+
+        //wait some more
+        _delay_ms(2000);
+    }
+
+
+
+}
